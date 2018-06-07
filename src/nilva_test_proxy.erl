497a6ca5abb0607f148577841eb2f69f62f11ecc
Eswar Paladugu <eswarp25@gmail.com>
2018-06-07 16:52:44 -0400

    Test proxy now drops/delays messages based on uniform distribution
    The proxy does not distinguish between incoming or outgoing messages
    when filtering them.
    
    You can add more distributions as needed. For example, you can use the
    “Box-Muller Transform” to get a normal distribution from uniform
    distribution.

--- src/nilva_test_proxy.erl
+++ src/nilva_test_proxy.erl
@@ -10,14 +10,20 @@
 
 % Types
 -type msg_type() :: 'incoming' | 'outgoing'.
--type proxy_mode() :: 'pass_all' | 'drop' | 'delay'.
--type drop_proxy_action(N) :: {'n_consecutive', N}
-                            % Drop N% randomly from the stream
-                            | {'uniform', N}.
--type delay_proxy_action(T) :: {'fixed', T}
-                             % Add delay based on uniform distribution
-                             | {'uniform', T}.
+-type proxy_action(T) :: 'pass' | 'drop' | {'delay', T}.
 
+-type pass_all() :: 'pass_all'.
+-type drop_fixed(N) :: {'drop', 'fixed', N}.
+-type drop_uniform(XPercent) :: {'drop', 'uniform', XPercent}.
+-type delay_fixed(T) :: {'delay', 'fixed', T}.
+-type delay_uniform(MinT, MaxT) :: {'delay', 'uniform', MinT, MaxT}.
+
+-type proxy_mode(N, XPercent, T, MinT, MaxT)
+                    :: pass_all()
+                    | drop_fixed(N)
+                    | drop_uniform(XPercent)
+                    | delay_fixed(T)
+                    | delay_uniform(MinT, MaxT).
 
 % Public API
 -export([start_link/0]).
@@ -61,7 +67,7 @@ handle_call({delay, uniform, T}, _, _) ->
 handle_cast({send_to, Node, Msg}, LoopData) ->
     {NewLoopData, ProxyAction} = filter_message(LoopData),
     case ProxyAction of
-        pass_all ->
+        pass->
             _Ignore = lager:info("Proxy routing message from ~p to ~p",
                                 [node(), Node]),
             % Send the message to proxy
@@ -83,7 +89,7 @@ handle_cast({send_to, Node, Msg}, LoopData) ->
 handle_cast(Msg, LoopData) ->
     {NewLoopData, ProxyAction} = filter_message(LoopData),
     case ProxyAction of
-        pass_all ->
+        pass ->
             _Ignore = lager:info("Proxy routing message to ~p",
                                 [node()]),
             % Note that `From` is embedded in the 4 raft message types
@@ -102,13 +108,29 @@ handle_cast(Msg, LoopData) ->
     end.
 
 
-filter_message(pass_all) -> {pass_all, pass_all};
+-spec filter_message(proxy_mode(N, XPercent, T, MinT, MaxT)) ->
+    {proxy_mode(N, XPercent, T, MinT, MaxT), proxy_action(T)}.
+filter_message(pass_all) -> {pass_all, pass};
 filter_message({drop, n_consecutive, N}) when N > 1, is_integer(N) ->
     {{drop, n_consecutive, N - 1}, drop};
 filter_message({drop, n_consecutive, N}) when N =:= 1, is_integer(N) ->
     {pass_all, drop};
+filter_message({drop, uniform, X}) when X > 0, X =< 100 ->
+    U = getUniformRand(0, 100),
+    case X < U of
+        true -> {{drop, uniform, X}, drop};
+        false -> {{drop, uniform, X}, pass}
+    end;
 filter_message({delay, fixed, T}) ->
-    {{delay, fixed, T}, {delay, T}}.
+    {{delay, fixed, T}, {delay, T}};
+filter_message({delay, uniform, MinT, MaxT}) ->
+    T = getUniformRand(MinT, MaxT),
+    {{delay, uniform, MinT, MaxT}, {delay, T}}.
+
+
+-spec getUniformRand(number(), number()) -> number().
+getUniformRand(Min, Max) ->
+    Min + (Max - Min) * rand:uniform().
 
 
 %% =========================================================================
