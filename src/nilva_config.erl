5dbd50ee30449dbd822395143706113e3d683ff5
Eswar Paladugu <eswarp25@gmail.com>
2018-10-03 23:21:56 -0400

    Rearranges the unit test order to match the function order

--- src/nilva_config.erl
+++ src/nilva_config.erl
@@ -84,6 +84,9 @@ validate_timeouts(H, Emin, Emax, CRT)
 %% Unit Tests
 %% =========================================================================
 
+% TODO: Setup a test for read_config/1
+
+
 num_required_for_quorum_test_() ->
     EvenPeers = ['p1', 'p2', 'p3', 'p4', 'p5', 'p6'],
     OddPeers = EvenPeers ++ ['p7'],
@@ -112,29 +115,6 @@ num_required_for_quorum_test_() ->
     ?_assertEqual(NQuorum, num_required_for_quorum(OddRaft))].
 
 
-validate_peers_test_() ->
-    ValidPeers = ['p1', 'p2', 'p3'],
-    InvalidPeers = ['p1', "p2", "p3"],
-    % InvalidPeers2 = {'p1', 'p2', 'p3'},
-    % Check if all the peers are atoms in a list
-    [?_assert(validate_peers(ValidPeers)),
-    ?_assertNot(validate_peers(InvalidPeers))].
-    % TODO: Dialyzer is not accepting this case
-    % ?_assertError(function_clause, validate_peers(InvalidPeers2))].
-
-
-validate_timeouts_test_() ->
-    % All timeouts are valid
-    {H, EMin, EMax, CRT} = {10, 100, 300, 3000},
-    % Heartbeat is not valid
-    InvalidH = 190,
-    % EMin > EMax
-    {InvalidEMin, InvalidEMax} = {1000, 300},
-    [?_assert(validate_timeouts(H, EMin, EMax, CRT)),
-    ?_assertNot(validate_timeouts(InvalidH, EMin, EMax, CRT)),
-    ?_assertNot(validate_timeouts(H, InvalidEMin, InvalidEMax, CRT))].
-
-
 convertToConfigRecord_test_() ->
     ValidProps = [
         {peers, ['p1', 'p2', 'p3']},
@@ -160,3 +140,26 @@ convertToConfigRecord_test_() ->
     [?_assert(is_record(convertToConfigRecord(ValidProps), raft_config)),
     ?_assertNot(is_record(convertToConfigRecord(InvalidPropsPeers), raft_config)),
     ?_assertNot(is_record(convertToConfigRecord(InvalidPropsTimeouts), raft_config))].
+
+
+validate_peers_test_() ->
+    ValidPeers = ['p1', 'p2', 'p3'],
+    InvalidPeers = ['p1', "p2", "p3"],
+    % InvalidPeers2 = {'p1', 'p2', 'p3'},
+    % Check if all the peers are atoms in a list
+    [?_assert(validate_peers(ValidPeers)),
+    ?_assertNot(validate_peers(InvalidPeers))].
+    % TODO: Dialyzer is not accepting this case
+    % ?_assertError(function_clause, validate_peers(InvalidPeers2))].
+
+
+validate_timeouts_test_() ->
+    % All timeouts are valid
+    {H, EMin, EMax, CRT} = {10, 100, 300, 3000},
+    % Heartbeat is not valid
+    InvalidH = 190,
+    % EMin > EMax
+    {InvalidEMin, InvalidEMax} = {1000, 300},
+    [?_assert(validate_timeouts(H, EMin, EMax, CRT)),
+    ?_assertNot(validate_timeouts(InvalidH, EMin, EMax, CRT)),
+    ?_assertNot(validate_timeouts(H, InvalidEMin, InvalidEMax, CRT))].
